7ce19dc8ca578f595073590b6ff5732c1d89a1f3
Rajveer Kania <rajveerkania02@gmail.com>
2025-08-28 18:12:37 +0530

    removed unnecessary files

--- src/app/api/layout-data/route.ts
+++ src/app/api/layout-data/route.ts
@@ -15,72 +15,44 @@ const MenuData = [
   },
   {
     id: 3,
-    title: "Projects",
-    path: "/projects",
-    newTab: false,
-  },
-  {
-    id: 4,
-    title: "Blog",
-    path: "/blog",
-    newTab: false,
-  },
-  {
-    id: 5,
-    title: "Services",
-    path: "/#services",
-    newTab: false,
-  },
-  {
-    id: 6,
     title: "Contact",
     path: "/contact",
     newTab: false,
   },
-  {
-    id: 7,
-    title: "Docs",
-    path: "/documentation",
-    newTab: false,
-  }
 ];
 
-
 const footerData = {
-    name: "Studiova",
-    tagline: "Build something together?",
-    info: [
-        {
-            icon: "/images/footer/email-arrow.svg",
-            link: "info@wrappixel.com",
-            href: "https://www.wrappixel.com/"
-        },
-        {
-            icon: "/images/footer/Location.svg",
-            link: "Zwolle Netherlands",
-            href: "https://maps.app.goo.gl/hpDp81fqzGt5y4bC8"
-        }
-    ],
-    links: [
-        { name: "Home", href: "/" },
-        { name: "About", href: "/about" },
-        { name: "Services", href: "/#services" },
-        { name: "Work", href: "/projects" },
-        { name: "Terms", href: "/terms-and-conditions" },
-        { name: "Privacy Policy", href: "/privacy-policy" },
-        { name: "Error 404", href: "/not-found" }
-    ],
-    socialLinks: [
-        { name: "Facebook", href: "https://www.facebook.com/" },
-        { name: "Instagram", href: "https://www.instagram.com/" },
-        { name: "Twitter", href: "https://x.com/" }
-    ],
-    copyright: "© Studiova copyright 2025"
+  name: "BCORE",
+  tagline: "GANDHINAGAR MARATHON 2026",
+  info: [
+    {
+      icon: "/images/footer/email-arrow.svg",
+      link: "bcore.rru.ac.in",
+      href: "https://bcore.rru.ac.in",
+    },
+    {
+      icon: "/images/footer/Location.svg",
+      link: "Bharat Centre of Olympic Research and Education",
+      href: "https://maps.app.goo.gl/VHnEHZ7R1bh9k22B8",
+    },
+  ],
+  links: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+    { name: "Error 404", href: "/not-found" },
+  ],
+  socialLinks: [
+    { name: "Facebook", href: "https://www.facebook.com/" },
+    { name: "Instagram", href: "https://www.instagram.com/" },
+    { name: "Twitter", href: "https://x.com/" },
+  ],
+  copyright: "© BCORE copyright 2025",
 };
 
 export const GET = async () => {
-    return NextResponse.json({
-        footerData,
-        MenuData
-    });
+  return NextResponse.json({
+    footerData,
+    MenuData,
+  });
 };
